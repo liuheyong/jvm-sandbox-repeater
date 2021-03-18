4d3b408a60680d6f989a8312751d18f5bd437bd7
liuheyong <17682347237@163.com>
2021-03-18 08:54:41 +0800

    2021年3月18日08:45:04

--- repeater-console/repeater-console-service/src/main/java/com/alibaba/repeater/console/service/impl/ReplayServiceImpl.java
+++ repeater-console/repeater-console-service/src/main/java/com/alibaba/repeater/console/service/impl/ReplayServiceImpl.java
@@ -51,7 +51,6 @@ public class ReplayServiceImpl implements ReplayService {
 
     @Value("${repeat.repeat.url}")
     private String repeatURL;
-
     @Resource
     private ModuleInfoService moduleInfoService;
     @Resource
@@ -107,9 +106,9 @@ public class ReplayServiceImpl implements ReplayService {
         Object actual;
         try {
             if (rm.getResponse() instanceof String) {
-                replay.setResponse(ConvertUtil.convert2Json((String)rm.getResponse()));
+                replay.setResponse(ConvertUtil.convert2Json((String) rm.getResponse()));
                 try {
-                    actual = JacksonUtil.deserialize((String)rm.getResponse(), Object.class);
+                    actual = JacksonUtil.deserialize((String) rm.getResponse(), Object.class);
                 } catch (SerializeException e) {
                     actual = rm.getResponse();
                 }
@@ -166,7 +165,7 @@ public class ReplayServiceImpl implements ReplayService {
         } catch (SerializeException e) {
             return RepeaterResult.builder().success(false).message(e.getMessage()).build();
         }
-        HttpUtil.Resp resp = HttpUtil.doPost(String.format(repeatURL,params.getIp(),params.getPort()), requestParams);
+        HttpUtil.Resp resp = HttpUtil.doPost(String.format(repeatURL, params.getIp(), params.getPort()), requestParams);
         if (resp.isSuccess()) {
             return RepeaterResult.builder().success(true).message("operate success").data(meta.getRepeatId()).build();
         }
