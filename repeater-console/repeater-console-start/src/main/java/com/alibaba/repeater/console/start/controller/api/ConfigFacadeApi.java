87f9c3ae374d7d50bb4920a8df3d7f69fcf66c98
liuheyong <17682347237@163.com>
2021-03-18 13:07:04 +0800

    2021年3月18日13:07:02

--- repeater-console/repeater-console-start/src/main/java/com/alibaba/repeater/console/start/controller/api/ConfigFacadeApi.java
+++ repeater-console/repeater-console-start/src/main/java/com/alibaba/repeater/console/start/controller/api/ConfigFacadeApi.java
@@ -1,15 +1,18 @@
 package com.alibaba.repeater.console.start.controller.api;
 
+import com.alibaba.jvm.sandbox.repeater.plugin.domain.Behavior;
 import com.alibaba.jvm.sandbox.repeater.plugin.domain.RepeaterConfig;
 import com.alibaba.jvm.sandbox.repeater.plugin.domain.RepeaterResult;
 import com.alibaba.repeater.console.common.domain.ModuleConfigBO;
 import com.alibaba.repeater.console.common.params.ModuleConfigParams;
 import com.alibaba.repeater.console.service.ModuleConfigService;
+import com.google.common.collect.Lists;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * {@link ConfigFacadeApi} Demo工程；作为repeater录制回放的配置管理服务
@@ -26,6 +29,8 @@ public class ConfigFacadeApi {
 
     /**
      * 配置文件拉取地址
+     * 在非 standalone 模式下，会从 repeater-console 的 /facade/api/config/${appName}/${env}接口中拉取配置。
+     * 在 standalone 模式下则读取~/.sandbox-module/cfg/repeater-config.json下的配置
      *
      * @param appName
      * @param env
@@ -39,5 +44,25 @@ public class ConfigFacadeApi {
         params.setEnvironment(env);
         RepeaterResult<ModuleConfigBO> result = moduleConfigService.query(params);
         return RepeaterResult.builder().success(result.isSuccess()).message(result.getMessage()).data(null == result.getData() ? null : result.getData().getConfigModel()).build();
+
+        //// 自己存配置；目前直接Mock了一份
+        //RepeaterConfig config = new RepeaterConfig();
+        //List<Behavior> behaviors = Lists.newArrayList();
+        //config.setPluginIdentities(Lists.newArrayList("http", "java-entrance", "java-subInvoke", "mybatis", "ibatis"));
+        //// 回放器
+        //config.setRepeatIdentities(Lists.newArrayList("java", "http"));
+        //// 白名单列表
+        //config.setHttpEntrancePatterns(Lists.newArrayList("^/regress/.*$"));
+        //// java入口方法
+        //behaviors.add(new Behavior("com.alibaba.repeater.console.service.impl.RegressServiceImpl", "getRegress"));
+        //config.setJavaEntranceBehaviors(behaviors);
+        //List<Behavior> subBehaviors = Lists.newArrayList();
+        //// java调用插件
+        //subBehaviors.add(new Behavior("com.alibaba.repeater.console.service.impl.RegressServiceImpl", "getRegressInner"));
+        //subBehaviors.add(new Behavior("com.alibaba.repeater.console.service.impl.RegressServiceImpl", "findPartner"));
+        //subBehaviors.add(new Behavior("com.alibaba.repeater.console.service.impl.RegressServiceImpl", "slogan"));
+        //config.setJavaSubInvokeBehaviors(subBehaviors);
+        //config.setUseTtl(true);
+        //return RepeaterResult.builder().success(true).message("operate success").data(config).build();
     }
 }
