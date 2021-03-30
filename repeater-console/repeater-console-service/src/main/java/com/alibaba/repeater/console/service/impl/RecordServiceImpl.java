03e155705268cd108682907116321fcbc5b5ff80
liuheyong <17682347237@163.com>
2021-03-30 16:57:35 +0800

    es  record 重构wc

--- repeater-console/repeater-console-service/src/main/java/com/alibaba/repeater/console/service/impl/RecordServiceImpl.java
+++ repeater-console/repeater-console-service/src/main/java/com/alibaba/repeater/console/service/impl/RecordServiceImpl.java
@@ -1,9 +1,11 @@
 package com.alibaba.repeater.console.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.alibaba.jvm.sandbox.repeater.plugin.core.wrapper.RecordWrapper;
 import com.alibaba.jvm.sandbox.repeater.plugin.core.wrapper.SerializerWrapper;
 import com.alibaba.jvm.sandbox.repeater.plugin.domain.RepeatModel;
 import com.alibaba.jvm.sandbox.repeater.plugin.domain.RepeaterResult;
+import com.alibaba.repeater.console.common.constant.Constant;
 import com.alibaba.repeater.console.common.domain.PageResult;
 import com.alibaba.repeater.console.common.domain.RecordBO;
 import com.alibaba.repeater.console.common.domain.RecordDetailBO;
@@ -13,12 +15,23 @@ import com.alibaba.repeater.console.dal.model.Record;
 import com.alibaba.repeater.console.service.RecordService;
 import com.alibaba.repeater.console.service.convert.ModelConverter;
 import com.alibaba.repeater.console.service.util.ConvertUtil;
+import com.alibaba.repeater.console.service.util.EsUtil;
+import com.alibaba.repeater.console.service.util.ResultHelper;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.domain.Page;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.ScoreSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -37,6 +50,8 @@ public class RecordServiceImpl implements RecordService {
     private ModelConverter<Record, RecordBO> recordConverter;
     @Resource
     private ModelConverter<Record, RecordDetailBO> recordDetailConverter;
+    @Resource
+    private EsUtil esUtil;
 
     @Override
     public RepeaterResult<String> saveRecord(String body) {
@@ -46,7 +61,7 @@ public class RecordServiceImpl implements RecordService {
                 return RepeaterResult.builder().success(false).message("invalid request").build();
             }
             Record record = ConvertUtil.convertWrapper(wrapper, body);
-            recordDao.insert(record);
+            esUtil.save(Constant.ES_INDEX, Constant.RECORD_ES_TYPE, record.getGmtRecord(), record);
             return RepeaterResult.builder().success(true).message("operate success").data("-/-").build();
         } catch (Throwable throwable) {
             return RepeaterResult.builder().success(false).message(throwable.getMessage()).build();
@@ -55,35 +70,88 @@ public class RecordServiceImpl implements RecordService {
 
     @Override
     public RepeaterResult<String> get(String appName, String traceId) {
-        Record record = recordDao.selectByAppNameAndTraceId(appName, traceId);
-        if (record == null) {
-            return RepeaterResult.builder().success(false).message("data not exits").build();
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()
+                .timeout(new TimeValue(5, TimeUnit.SECONDS))
+                .query(QueryBuilders.termsQuery("appName", appName))
+                .query(QueryBuilders.termsQuery("traceId", traceId))
+                .sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        List<Map<String, Object>> search = esUtil.search(Constant.ES_INDEX, Constant.MODULE_INFO_ES_TYPE, sourceBuilder);
+        List<Record> objectList = search.stream()
+                .map(o -> BeanUtil.mapToBean(o, Record.class, true))
+                .collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(objectList) || objectList.get(0) == null) {
+            return ResultHelper.fail("data not exist");
         }
-        return RepeaterResult.builder().success(true).message("operate success").data(record.getWrapperRecord()).build();
+        return RepeaterResult.builder().success(true).message("operate success").data(objectList.get(0).getWrapperRecord()).build();
     }
 
     @Override
     public PageResult<RecordBO> query(RecordParams params) {
-        Page<Record> page = recordDao.selectByAppNameOrTraceId(params);
-        PageResult<RecordBO> result = new PageResult<>();
-        if (page.hasContent()) {
-            result.setSuccess(true);
-            result.setCount(page.getTotalElements());
-            result.setTotalPage(page.getTotalPages());
-            result.setPageIndex(params.getPage());
-            result.setPageSize(params.getSize());
-            result.setData(page.getContent().stream().map(recordConverter::convert).collect(Collectors.toList()));
+        if (!esUtil.indexExists(Constant.ES_INDEX)) {
+            PageResult<RecordBO> pageResult = new PageResult<>();
+            pageResult.setSuccess(false);
+            pageResult.setMessage("no such data: " + Constant.ES_INDEX);
+            return pageResult;
         }
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()
+                .from((params.getPage() - 1) * params.getSize())
+                .size(params.getSize())
+                .timeout(new TimeValue(5, TimeUnit.SECONDS))
+                .sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        if (StringUtils.isNotBlank(params.getAppName())) {
+            sourceBuilder.query(QueryBuilders.termsQuery("appName", params.getAppName()));
+        }
+        if (StringUtils.isNotBlank(params.getTraceId())) {
+            sourceBuilder.query(QueryBuilders.termsQuery("traceId", params.getTraceId()));
+        }
+        List<Map<String, Object>> search = esUtil.search(Constant.ES_INDEX, Constant.RECORD_ES_TYPE, sourceBuilder);
+        List<Record> objectList = search.stream()
+                .map(o -> BeanUtil.mapToBean(o, Record.class, true))
+                .collect(Collectors.toList());
+
+        SearchSourceBuilder sourceBuilder2 = new SearchSourceBuilder()
+                .timeout(new TimeValue(5, TimeUnit.SECONDS))
+                .sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        List<Map<String, Object>> search2 = esUtil.search(Constant.ES_INDEX, Constant.RECORD_ES_TYPE, sourceBuilder2);
+
+        PageResult<RecordBO> result = new PageResult<>();
+        if (CollectionUtils.isNotEmpty(objectList)) {
+            result.setCount(Long.valueOf(search2.size()));
+            result.setTotalPage((search2.size() - 1) / params.getSize() + 1);
+            result.setData(objectList.stream().map(recordConverter::convert).collect(Collectors.toList()));
+        } else {
+            result.setCount(0L);
+            result.setTotalPage(0);
+            result.setData(Lists.newArrayList());
+        }
+        result.setSuccess(true);
+        result.setPageIndex(params.getPage());
+        result.setPageSize(params.getSize());
         return result;
     }
 
     @Override
     public RepeaterResult<RecordDetailBO> getDetail(RecordParams params) {
-        Record record = recordDao.selectByAppNameAndTraceId(params.getAppName(), params.getTraceId());
-        if (record == null) {
-            return RepeaterResult.builder().message("data not found").build();
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()
+                .timeout(new TimeValue(5, TimeUnit.SECONDS))
+                .sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        if (StringUtils.isNotBlank(params.getAppName())) {
+            sourceBuilder.query(QueryBuilders.termsQuery("appName", params.getAppName()));
         }
-        return RepeaterResult.builder().success(true).data(recordDetailConverter.convert(record)).build();
+        if (StringUtils.isNotBlank(params.getTraceId())) {
+            sourceBuilder.query(QueryBuilders.termsQuery("traceId", params.getTraceId()));
+        }
+        if (StringUtils.isNotBlank(params.getEnvironment())) {
+            sourceBuilder.query(QueryBuilders.termsQuery("environment", params.getEnvironment()));
+        }
+        List<Map<String, Object>> search = esUtil.search(Constant.ES_INDEX, Constant.MODULE_INFO_ES_TYPE, sourceBuilder);
+        List<Record> objectList = search.stream()
+                .map(o -> BeanUtil.mapToBean(o, Record.class, true))
+                .collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(objectList) || objectList.get(0) == null) {
+            return ResultHelper.fail("data not exist");
+        }
+        return RepeaterResult.builder().success(true).data(recordDetailConverter.convert(objectList.get(0))).build();
     }
 
     @Override
