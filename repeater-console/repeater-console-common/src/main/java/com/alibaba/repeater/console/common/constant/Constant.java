de3d349a1e38556ff2d4c7928fecab01025370f1
liuheyong <17682347237@163.com>
2021-04-05 00:17:58 +0800

    fix es

--- repeater-console/repeater-console-common/src/main/java/com/alibaba/repeater/console/common/constant/Constant.java
+++ repeater-console/repeater-console-common/src/main/java/com/alibaba/repeater/console/common/constant/Constant.java
@@ -11,41 +11,41 @@ public class Constant {
     /**
      * es ModuleConfig index
      */
-    public static final String MODULE_CONFIG_ES_INDEX = "module_config_es_index";
+    public static final String MODULE_CONFIG_ES_INDEX = "repeater_module_config_es_index";
 
     /**
      * es ModuleConfig type
      */
-    public static final String MODULE_CONFIG_ES_TYPE = "module_config_es_type";
+    public static final String MODULE_CONFIG_ES_TYPE = "repeater_module_config_es_type";
 
     /**
      * es ModuleInfo index
      */
-    public static final String MODULE_INFO_ES_INDEX = "module_info_es_index";
+    public static final String MODULE_INFO_ES_INDEX = "repeater_module_info_es_index";
 
     /**
      * es ModuleInfo type
      */
-    public static final String MODULE_INFO_ES_TYPE = "module_info_es_type";
+    public static final String MODULE_INFO_ES_TYPE = "repeater_module_info_es_type";
 
     /**
      * es Record index
      */
-    public static final String RECORD_ES_INDEX = "record_es_index";
+    public static final String RECORD_ES_INDEX = "repeater_record_es_index";
 
     /**
      * es Record type
      */
-    public static final String RECORD_ES_TYPE = "record_es_type";
+    public static final String RECORD_ES_TYPE = "repeater_record_es_type";
 
     /**
      * es Replay index
      */
-    public static final String REPLAY_ES_INDEX = "replay_es_index";
+    public static final String REPLAY_ES_INDEX = "repeater_replay_es_index";
 
     /**
      * es Replay type
      */
-    public static final String REPLAY_ES_TYPE = "replay_es_type";
+    public static final String REPLAY_ES_TYPE = "repeater_replay_es_type";
 
 }
